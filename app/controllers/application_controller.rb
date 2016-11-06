9f62e2f375a4096897bd28195d3110ff64c66376
minigt <minigtraverso@gmail.com>
2016-11-06 15:03:13 -0300

    loging development

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def after_sign_in_path_for(resource_or_scope)
-    redirect_to '/usuarios'
+    if current_usuario.rol == 'user'
+       return '/usuario'
+     else
+       return '/admin'
+     end
   end
 
   def after_sign_out_path_for(resource_or_scope)
