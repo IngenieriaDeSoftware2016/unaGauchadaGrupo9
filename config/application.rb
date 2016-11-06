0fc9d99e87ada0c856613c8d237e037afe3505ce
minigt <minigtraverso@gmail.com>
2016-11-06 19:59:52 -0300

    config changes

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-require "devise"
+require 'devise'
 Bundler.require(*Rails.groups)
 
 module UnaGauchadaGrupo9
