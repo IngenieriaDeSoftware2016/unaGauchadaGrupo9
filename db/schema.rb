e8b59d1f716794279f7956732304bf6b0afd4391
svaldo1990 <santiagovaldovinos1990@gmail.com>
2016-11-09 08:18:24 -0300

    backup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161106220615) do
 
   # These are extensions that must be enabled in order to support this database
@@ -32,29 +31,5 @@ ActiveRecord::Schema.define(version: 20161106220615) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "usuarios", force: :cascade do |t|
-    t.string   "email",                              default: "",    null: false
-    t.string   "encrypted_password",                 default: "",    null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,     null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.string   "nombre",                 limit: 255
-    t.integer  "edad"
-    t.string   "localidad",              limit: 255
-    t.integer  "puntaje"
-    t.string   "telefono",               limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
-    t.boolean  "admin",                              default: false
-  end
-
-  add_index "usuarios", ["email"], name: "index_usuarios_on_email", unique: true, using: :btree
-  add_index "usuarios", ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true, using: :btree
 
 end
