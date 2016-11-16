dd7a6c8a9b6851520f98595389e514f6e7e26b5b
minigt <minigtraverso@gmail.com>
2016-11-16 15:10:46 -0300

    uniqness of puntaje and nombre set

--- app/models/logro.rb
+++ app/models/logro.rb
@@ -1,4 +1,4 @@
 class Logro < ActiveRecord::Base
     validates :puntaje, uniqueness: true
-    validates :nombre, uniqueness: true
+    validates :nombre, uniqueness: {message: "El nombre seleccionado ya existe"}
 end
