7271b4dfd0a14c13bbe9e8272034570a0152c294
minigt <minigtraverso@gmail.com>
2016-11-16 22:01:59 -0300

    mail fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'una-gauchada-grupo9.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => "una-gauchada-grupo9.herokuapp.com" }
 
 end
