f2a017ca97019090cf686db64b6c624ae325ed51
minigt <minigtraverso@gmail.com>
2016-11-18 08:48:07 -0300

    aloud acces to gauchada to anyone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :authenticate_usuario!, :except => [:index]
+  before_filter :authenticate_usuario!, :except => [:index]
 
   def after_sign_in_path_for(resource_or_scope)
     if current_usuario.admin
