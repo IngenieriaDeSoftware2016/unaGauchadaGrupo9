4cbb287a798c319acb81cfffabd3e7863dc0f44d
minigt <minigtraverso@gmail.com>
2016-11-18 08:51:46 -0300

    aloud acces to gauchada to anyone

--- app/controllers/gauchadas_controller.rb
+++ app/controllers/gauchadas_controller.rb
@@ -1,6 +1,6 @@
 class GauchadasController < ApplicationController
   #skip_before_filter :authenticate_user!#, :only => :gauchadas_path
-  skip_before_filter :authenticate_user!, :only => :show
+  skip_before_filter :authenticate_usuario!, :only => :show
   before_filter      :authenticate_user!, :only => :show
   def index
 
