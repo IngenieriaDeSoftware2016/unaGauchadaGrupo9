7f8d00913efafdbaea81ae35a95441d93da35b71
minigt <minigtraverso@gmail.com>
2016-11-18 16:37:35 -0300

    user edit fixed

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,27 +1,12 @@
 class RegistrationsController < Devise::RegistrationsController
 
-  def new
-    build_resource({})
-    usuario = Usuario.new
-    respond_with usuario
-  end
-
-  before_filter :configure_permitted_parameters
-
-  protected
+  private
 
   # my custom fields are :name, :heard_how
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) do |u|
-      u.permit(:nombre,:edad ,:rol,:telefono,:localidad,:puntaje,
-        :email, :password, :password_confirmation)
-    end
-    devise_parameter_sanitizer.for(:account_update) do |u|
-      u.permit(:nombre,:edad ,:rol,:telefono,:localidad,:puntaje,
-        :email, :password, :password_confirmation, :current_password)
-    end
+  def account_update_params
+    allow = [:email, :password, :password_confirmation, :nombre,:edad ,:rol,:telefono,:localidad,:puntaje]
+    params.require(resource_name).permit(allow)
   end
-  private
 
   def sign_up_params
     allow = [:email, :password, :password_confirmation, :nombre,:edad ,:rol,:telefono,:localidad,:puntaje]
