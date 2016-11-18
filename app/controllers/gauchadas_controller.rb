d371e351ae0f48262d1d6b4490c957d1971765db
minigt <minigtraverso@gmail.com>
2016-11-18 08:10:29 -0300

    aloud acces to gauchada to anyone

--- app/controllers/gauchadas_controller.rb
+++ app/controllers/gauchadas_controller.rb
@@ -1,10 +1,14 @@
 class GauchadasController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:show, :index]
+  skip_before_action :authenticate_user!, except: [:edit]
   def index
 
   end
 
   def show
     @gauchada = Gauchada.find(params[:id])
+  end
+
+  def edit
+
   end
 end
