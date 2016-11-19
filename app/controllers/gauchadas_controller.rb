be76086609a2f3e79129a188fc7911eb4cf10533
minigt <minigtraverso@gmail.com>
2016-11-19 09:24:11 -0300

    metod acces change

--- app/controllers/gauchadas_controller.rb
+++ app/controllers/gauchadas_controller.rb
@@ -47,22 +47,23 @@ class GauchadasController < ApplicationController
   end
 
 
+  def filtered
+     gauchada = Gauchada.arel_table
+     if params[:palabra] && params[:localidad]
+       self.where("descripcion like '?%' ", params[:palabra]).where("localidad like '?%' ", params[:localidad])
+     elsif params[:palabra] && !params[:localidad]
+       self.where("descripcion like '?%' ", params[:palabra])
+     elsif !params[:palabra] && params[:localidad]
+       self.where("localidad like '?%' ", params[:localidad])
+     else
+       self.all
+     end
+  end
+  
 private
 
  def gauchada_params
     params.require(:gauchada).permit(:usuario_id, :creacion, :nombre, :descripcion, :localidad, :vencimiento)
  end
 
- def filtered
-    gauchada = Gauchada.arel_table
-    if params[:palabra] && params[:localidad]
-      self.where("descripcion like '?%' ", params[:palabra]).where("localidad like '?%' ", params[:localidad])
-    elsif params[:palabra] && !params[:localidad]
-      self.where("descripcion like '?%' ", params[:palabra])
-    elsif !params[:palabra] && params[:localidad]
-      self.where("localidad like '?%' ", params[:localidad])
-    else
-      self.all
-    end
-   end
 end
