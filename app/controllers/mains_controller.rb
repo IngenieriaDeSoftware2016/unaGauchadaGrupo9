bbc5dea496bdc79e6ba985ea3ab57fc7ade541c9
minigt <minigtraverso@gmail.com>
2016-11-19 09:05:44 -0300

    seacrh bar fix

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -6,6 +6,26 @@ class MainsController < ApplicationController
   end
 
   def search
+    @gauchadas.filtered_by_key_word(:palabra).filtered_by_localidad(:localidad);
+    
   end
+
+  private
+  def self.filtered_by_key_word(opts = {})
+     gauchada = Gauchada.arel_table
+     if params[:palabra]
+       self.where("descripcion like '?%' ", params[:palabra])
+     else
+       self.all
+     end
+   end
+   def self.filtered_by_localidad(opts = {})
+     gauchada = Gauchada.arel_table
+     if params[:localidad]
+       self.where("descripcion like '?%' ", params[:localidad])
+     else
+       self.all
+     end
+    end
 
 end
