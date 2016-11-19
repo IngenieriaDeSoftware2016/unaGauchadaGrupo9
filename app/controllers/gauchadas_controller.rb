4d837fee2c99a64914376502636bc52a13255c2b
minigt <minigtraverso@gmail.com>
2016-11-19 09:21:34 -0300

    metod moved from main controller to gauchadas controler

--- app/controllers/gauchadas_controller.rb
+++ app/controllers/gauchadas_controller.rb
@@ -52,4 +52,17 @@ private
  def gauchada_params
     params.require(:gauchada).permit(:usuario_id, :creacion, :nombre, :descripcion, :localidad, :vencimiento)
  end
+
+ def self.filtered
+    gauchada = Gauchada.arel_table
+    if params[:palabra] && params[:localidad]
+      self.where("descripcion like '?%' ", params[:palabra]).where("localidad like '?%' ", params[:localidad])
+    elsif params[:palabra] && !params[:localidad]
+      self.where("descripcion like '?%' ", params[:palabra])
+    elsif !params[:palabra] && params[:localidad]
+      self.where("localidad like '?%' ", params[:localidad])
+    else
+      self.all
+    end
+   end
 end
--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -7,20 +7,7 @@ class MainsController < ApplicationController
 
   def search
     @gauchadas.filtered;
-
   end
 
-  private
-  def self.filtered
-     gauchada = Gauchada.arel_table
-     if params[:palabra] && params[:localidad]
-       self.where("descripcion like '?%' ", params[:palabra]).where("localidad like '?%' ", params[:localidad])
-     elsif params[:palabra] && !params[:localidad]
-       self.where("descripcion like '?%' ", params[:palabra])
-     elsif !params[:palabra] && params[:localidad]
-       self.where("localidad like '?%' ", params[:localidad])
-     else
-       self.all
-     end
-    end
+  
 end
