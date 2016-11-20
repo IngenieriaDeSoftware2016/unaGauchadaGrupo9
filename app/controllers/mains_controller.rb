5c7f1079873941b528cb21cec0a9a27a2e191bf8
minigt <minigtraverso@gmail.com>
2016-11-20 11:09:38 -0300

    search fix

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -8,12 +8,11 @@ class MainsController < ApplicationController
   def search
     if(params[:localidad]!=""&&params[:palabra]!="")
       @gauchadas=Gauchada.where("localidad ilike ?", "%" + params[:localidad] + "%").where("descripcion ilike ?", "%" + params[:palabra] + "%")
-    elsif(params[:localidad]&&!params[:palabra])
+    elsif(params[:localidad]!=""&&params[:palabra]=="")
       @gauchadas=Gauchada.where("localidad ilike ?", "%" + params[:localidad] + "%")
-    elsif(!params[:localidad]&&params[:palabra])
+    elsif(params[:localidad]==""&&params[:palabra]!="")
       palabras=params[:palabra].split(/\s* \s*/).map { |s| s.gsub(/[_%]/, '%' => '\\%', '_' => '\\_') }.map { |s| '%' + s + '%' }
-      p=palabras.split(/\s* \s*/).map { |s| s.gsub(/[_%]/, '%' => '\\%', '_' => '\\_') }.map { |s| '%' + s + '%' }
-      @gauchadas=Gauchada.where('descripcion ilike any(array[?])', p)
+      @gauchadas=Gauchada.where('descripcion ilike any(array[?])', palabras)
       #@gauchadas=Gauchada.where("descripcion ilike ?", "%" + palabras + "%")
     else
       @gauchadas=Gauchada.all
