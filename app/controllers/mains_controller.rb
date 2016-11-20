23c2287774e30982a7beeaecde9366f0cf694c56
minigt <minigtraverso@gmail.com>
2016-11-20 11:01:43 -0300

    search fix

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -11,8 +11,9 @@ class MainsController < ApplicationController
     elsif(params[:localidad]&&!params[:palabra])
       @gauchadas=Gauchada.where("localidad ilike ?", "%" + params[:localidad] + "%")
     elsif(!params[:localidad]&&params[:palabra])
-      #palabras=params[:palabra].split(/\s* \s*/).map { |s| s.gsub(/[_%]/, '%' => '\\%', '_' => '\\_') }.map { |s| '%' + s + '%' }
-      @gauchadas=Gauchada.where('descripcion ilike any(array[?])', params[:palabra])
+      palabras=params[:palabra].split(/\s* \s*/).map { |s| s.gsub(/[_%]/, '%' => '\\%', '_' => '\\_') }.map { |s| '%' + s + '%' }
+      p=palabras.split(/\s* \s*/).map { |s| s.gsub(/[_%]/, '%' => '\\%', '_' => '\\_') }.map { |s| '%' + s + '%' }
+      @gauchadas=Gauchada.where('descripcion ilike any(array[?])', p)
       #@gauchadas=Gauchada.where("descripcion ilike ?", "%" + palabras + "%")
     else
       @gauchadas=Gauchada.all
