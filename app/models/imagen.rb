d2b18ada4a12cebfd8aef02b6f208d1a8dc5adcb
minigt <minigtraverso@gmail.com>
2016-11-21 12:19:02 -0300

    adding image

--- app/models/imagen.rb
+++ app/models/imagen.rb
@@ -1,10 +1,8 @@
 class Imagen < ActiveRecord::Base
-  attr_accessible :picture, :verified, :usuario_id, :gauchada_id
 #added for paperclip-drop gem
 has_attached_file :picture,
  :storage => :dropbox,
-:dropbox_credentials => "#{Rails.root}/config/dropbox_c
-onfig.yml",
+:dropbox_credentials => "#{Rails.root}/config/dropbox_config.yml",
  :styles => { :medium => "300x300" , :thumb => "100x100>"},
 :dropbox_options => {
 :path => proc { |style| "#{style}/#{id}_#{picture.original_filename}"},       :unique_filename => true
