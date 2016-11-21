ae74eedd6c27a68faf7df6a81c05c933cda5f2b0
minigt <minigtraverso@gmail.com>
2016-11-21 17:10:55 -0300

    logro on user show fix

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -7,6 +7,13 @@ class UsuariosController < ApplicationController
 
   def show
     @usuario = Usuario.find(current_usuario.id)
+    @logro=Logro.first
+    Logro.all.each do |l|
+      if(@usuario.puntaje>@logro.puntaje)
+        @logro=l
+        end
+      end
+    end
   end
 
   def edit
