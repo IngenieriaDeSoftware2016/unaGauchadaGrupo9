0303df4f5b83f86ae0cda5dc5666536ec9aa4947
minigt <minigtraverso@gmail.com>
2016-11-22 16:43:24 -0300

    mail config change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,7 @@ Rails.application.configure do
   #:authentication => :cram_md5
   :user_name => 'unagauchada@yahoo.com',
   :password => 'villaMirasol09',
+  :domain => 'smtp.yahoo.com',
   :address => 'smtp.yahoo.com',
   :port => '25',
   :authentication => :plain
