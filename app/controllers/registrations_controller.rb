2e8ab93ea84fccc1ee5aaff098ca77776fec34e5
svaldo1990 <santiagovaldovinos1990@gmail.com>
2016-11-22 14:50:44 -0300

    redirect

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
 
+  def edit
+    super()
+    redirect_to usuario_path
+  end
+  
   private
 
   # my custom fields are :name, :heard_how
@@ -12,5 +17,7 @@ class RegistrationsController < Devise::RegistrationsController
     allow = [:email, :password, :password_confirmation, :nombre,:edad ,:rol,:telefono,:localidad,:puntaje]
     params.require(resource_name).permit(allow)
   end
+
+
 
 end
