a0a8cc875d53ba1bda34bc8fb7bc86b43661cb4d
minigt <minigtraverso@gmail.com>
2016-11-23 09:06:13 -0300

    ususario load image fix

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -25,7 +25,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
     def after_update_path_for(resource)
-      user_path(resource)
+      usuario_path(resource)
     end
 
 
