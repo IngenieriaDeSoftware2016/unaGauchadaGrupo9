d7c1264de8307fdb701516465099022d6fb0b88c
minigt <minigtraverso@gmail.com>
2016-11-23 08:39:51 -0300

    user image fix

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,12 +11,12 @@ class RegistrationsController < Devise::RegistrationsController
 
   # my custom fields are :name, :heard_how
   def account_update_params
-    allow = [:email, :password, :password_confirmation,:current_password, :nombre,:edad ,:rol,:telefono,:localidad,:puntaje]
+    allow = [:email, :password, :password_confirmation,:current_password, :nombre,:edad ,:rol,:telefono,:localidad,:puntaje,:imagen]
     params.require(resource_name).permit(allow)
   end
 
   def sign_up_params
-    allow = [:email, :password, :password_confirmation, :nombre,:edad ,:rol,:telefono,:localidad,:puntaje]
+    allow = [:email, :password, :password_confirmation, :nombre,:edad ,:rol,:telefono,:localidad,:puntaje,:imagen]
     params.require(resource_name).permit(allow)
   end
 
