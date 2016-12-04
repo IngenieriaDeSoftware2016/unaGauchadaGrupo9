62c0fc02a48af3d0792688a378dcd5cc9d1fb59d
minigt <minigtraverso@gmail.com>
2016-12-04 20:53:17 -0300

    numero_tarjeta fix

--- db/migrate/20161204234618_change_numero_tarjeta_format5_in_compras.rb
+++ db/migrate/20161204234618_change_numero_tarjeta_format5_in_compras.rb
@@ -1,5 +1,5 @@
 class ChangeNumeroTarjetaFormat5InCompras < ActiveRecord::Migration
-  def down
-      remove_column :compras, :numero_tarjeta
+  def up
+      add_column :compras, :numero_tarjeta,:bigint
   end
 end
