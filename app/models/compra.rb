abc9199de820649e5386e288b570a6e0b0a903ff
Santiago Valdovinos <Santiago@MacBook-Pro-de-Santiago.local>
2016-12-04 20:37:45 -0300

    compras

--- app/models/compra.rb
+++ app/models/compra.rb
@@ -1,3 +1,9 @@
 class Compra < ActiveRecord::Base
   belongs_to :usuario
+
+  validates :numero_trajeta, :presence => true,
+                    :length => { :minimum => 16 }
+  validates :cs_trajeta, :presence => true,
+                    :length => { :minimum => 3 }
+
 end
