ac1b5077ca4cce6513d628731a7ffdb07c2e43fe
minigt <minigtraverso@gmail.com>
2016-12-04 21:12:22 -0300

    numero_tarjeta fix

--- app/models/compra.rb
+++ app/models/compra.rb
@@ -1,9 +1,9 @@
 class Compra < ActiveRecord::Base
   belongs_to :usuario
 
-  validates :numero_trajeta, :presence => true,
+  validates :numero_tarjeta, :presence => true,
                     :length => { :minimum => 16 }
-  validates :cs_trajeta, :presence => true,
+  validates :cs_tarjeta, :presence => true,
                     :length => { :minimum => 3 }
 
 end
